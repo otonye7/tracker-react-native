24eeac9973b32b98a9cf8c7e791c25a1d4a10c94
otonye <otonyeamie@gmail.com>
2021-05-13 20:52:25 +0100

    first commit

--- App.js
+++ App.js
@@ -5,17 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>I am a boy</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {  
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    padding: 50,
+  }
 });
